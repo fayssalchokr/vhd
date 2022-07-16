b469767952851398db0e7babe50fbe297a149697
Fayssal Chokri <fayssalchokri_revonsadmin@protonmail.com>
2022-07-15 17:53:35 -0700

    Just a update

--- Application.cpp
+++ Application.cpp
@@ -5,3 +5,58 @@
 #include "DiskImages/DiskImage.h"
 #include "FileSystems/FileSystem.h"
 #include "MBR.h"
+int main(int argc, char** argv)
+{
+    ArgParser args;
+    args.add_param("mbr", 'm', "Path to an MBR (Master Boot Record)", false)
+        .add_param("filesystem", 'x', "Filesystem to use followed by <,option=value>")
+        .add_list("files", 'f', "Paths to additional files to be put inside root directory")
+        .add_list("store", 't', "List of <file>,<sector> to store outside of the filesystem")
+        .add_param("directory", 'd', "Path to the root directory for this disk (copied recursively)")
+        .add_param("size", 's', "Hard disk size to be generated (in megabytes)")
+        .add_param("image-format", 'g', "Generated image format, currently only valid is vmdk")
+        .add_param("image-directory", 'i', "Path to a directory to output image files")
+        .add_param("image-name", 'n', "Name of the image to be generated")
+        .add_param("part-align", 'p', "Partition alignment (in 512 byte sectors)")
+        .add_flag("verbose", 'v', "Enable verbose logging")
+        .add_help("help", 'h', "Display this menu and exit",
+                  [&]() { std::cout << "How to use VirtualHDDCreator:\n" << args; exit(1); });
+
+    try {
+        args.parse(argc, argv);
+
+        if (args.is_set("verbose"))
+            Logger::the().set_level(Logger::Level::INFO);
+
+        auto image_size = args.get_uint_or("size", 64) * MB;
+
+         // align for sector size
+        image_size = image_size - (image_size % DiskImage::sector_size);
+        auto image_sector_count = image_size / DiskImage::sector_size;
+
+        auto image_dir = std::filesystem::current_path().string();
+        if (args.is_set("image-directory"))
+            image_dir = args.get("image-directory");
+        auto image_name = args.get_or("image-name", "MyHDD");
+
+        auto image_format = args.get_or("image-format", "vmdk");
+        auto image = DiskImage::create(image_format, image_dir, image_name, image_size);
+
+        auto partition_alignment = args.get_uint_or("part-align", DiskImage::partition_alignment);
+        MBR mbr(args.get("mbr"), image->geometry(), partition_alignment);
+
+        MBR::Partition partition_1(image_sector_count - partition_alignment);
+        auto partition_offset = mbr.add_partition(partition_1);
+        mbr.write_into(*image);
+
+        auto fs = FileSystem::create(*image, partition_offset, partition_1.sector_count(), args);
+
+        FSObject obj {};
+
+        auto path_rooted_at = [](const std::string& root, const std::string& sub_directory_path) -> std::string
+        {
+            auto rooted_path = std::filesystem::path("/");
+            rooted_path = rooted_path / std::filesystem::path(std::string(sub_directory_path.data() + root.size()));
+
+            return rooted_path.string();
+        };
