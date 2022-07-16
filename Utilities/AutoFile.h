202b587bc18eee9b4f3cf42f1458ddd604d5e934
Fayssal Chokri <fayssalchokri_revonsadmin@protonmail.com>
2022-07-16 06:30:11 -0700

    Make sure paths are always null-terminated, GenericUnix->POSIX

---
+++ Utilities/AutoFile.h
@@ -0,0 +1,89 @@
+#pragma once
+
+#include <string_view>
+#include <vector>
+
+class AutoFile
+{
+public:
+    enum Mode {
+        READ = 1,
+        WRITE = 2,
+        TRUNCATE = 4
+    };
+
+    friend Mode operator|(Mode l, Mode r)
+    {
+        return static_cast<Mode>(static_cast<int>(l) | static_cast<int>(r));
+    }
+
+    AutoFile()
+        : m_platform_handle(nullptr)
+    {
+    }
+
+    AutoFile(const std::string& path, Mode mode)
+        : m_platform_handle(nullptr)
+    {
+        open(path, mode);
+    }
+
+    AutoFile(const char* path, Mode mode)
+    {
+        open(path, mode);
+    }
+
+    AutoFile(const AutoFile& other_file) = delete;
+    AutoFile& operator=(const AutoFile& other_file) = delete;
+
+    AutoFile(AutoFile&& other_file) noexcept
+        : m_platform_handle(other_file.m_platform_handle)
+    {
+        other_file.m_platform_handle = nullptr;
+    }
+
+    AutoFile& operator=(AutoFile&& other_file) noexcept
+    {
+        std::swap(m_platform_handle, other_file.m_platform_handle);
+
+        return *this;
+    }
+
+    void open(const char* path, Mode mode);
+    void open(const std::string& path, Mode mode) { return open(path.data(), mode); }
+
+    size_t size() const;
+    size_t offset() const;
+
+    void write(std::string_view data)
+    {
+        write(data.data(), data.size());
+    }
+
+    void write(const char* data, size_t size)
+    {
+        write(reinterpret_cast<const uint8_t*>(data), size);
+    }
+
+    void write(const uint8_t* data, size_t size);
+    void read(uint8_t* into, size_t size);
+
+    size_t set_offset(size_t offset);
+    size_t skip(size_t bytes);
+    void set_size(size_t new_size);
+
+    ~AutoFile();
+
+private:
+    void* m_platform_handle;
+};
+
+inline std::vector<uint8_t> read_entire(const std::string& path)
+{
+    AutoFile f(path, AutoFile::READ);
+
+    std::vector<uint8_t> data(f.size());
+    f.read(data.data(), data.size());
+
+    return data;
+}
