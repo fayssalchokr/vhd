14746326356bcc54a2aa901d6bb6836accc42030
Fayssal Chokri <fayssalchokri_revonsadmin@protonmail.com>
2022-07-15 17:50:56 -0700

    Make sure paths are always null-terminated, GenericUnix->POSIX

--- MBR.cpp
+++ MBR.cpp
@@ -55,3 +55,51 @@ void MBR::Partition::serialize(uint8_t* into, const DiskGeometry& geometry, size
     memcpy(&into[8], &lba_offset, sizeof(uint32_t));
     memcpy(&into[12], &m_sector_count, sizeof(uint32_t));
 }
+uint32_t MBR::Partition::sector_count() const noexcept
+{
+    return m_sector_count;
+}
+
+MBR::MBR(const std::string& path, const DiskGeometry& geometry, size_t offset_of_first_partition)
+    : m_mbr{}
+    , m_active_partition(0)
+    , m_DiskGeometry(geometry)
+    , m_active_lba_offset(offset_of_first_partition)
+    , m_initial_lba_offset(offset_of_first_partition)
+{
+    AutoFile mbr_file(path, AutoFile::READ);
+
+    mbr_file.read(m_mbr, mbr_size);
+
+    validate_mbr();
+}
+
+void MBR::write_into(DiskImage& image)
+{
+    image.write_at(m_mbr, mbr_size, 0);
+}
+
+size_t MBR::add_partition(const Partition& partition)
+{
+    size_t partition_offset = partition_base + (m_active_partition * partition_entry_size);
+
+    size_t partition_lba_offset = m_active_lba_offset;
+
+    partition.serialize(&m_mbr[partition_offset], m_DiskGeometry, partition_lba_offset);
+
+    m_active_lba_offset += partition.sector_count();
+    m_active_partition  += 1;
+
+    return partition_lba_offset;
+}
+
+void MBR::validate_mbr()
+{
+    uint8_t expected_partition_data[mpt_size]{};
+
+    if (memcmp(&m_mbr[partition_base], expected_partition_data, mpt_size))
+        throw std::runtime_error("detected a non-empty MPT");
+
+    if (m_mbr[510] != 0x55 && m_mbr[511] != 0xAA)
+        throw std::runtime_error("incorrect bootsector signature, has to end with 0x55AA");
+}
